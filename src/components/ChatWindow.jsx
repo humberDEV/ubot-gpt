7b58b72d0873251840dd1829adfc6a0be5062560
humberDEV <humberbrawl@gmail.com>
2024-06-14 22:13:28 +0200

    refactor some components and responsive :wranch:

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,22 +1,43 @@
 import "../styles/chat-window.css";
-
+import { useState } from "react";
 import Chat from "./Chat";
 
 export default function ChatWindow() {
-  // function addMessage(message, user) {
-  //   setMessage("");
-  //   setUser("me");
-  // }
+  const [messageArray] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  function addMessageUser() {
+    if (newMessage.trim()) {
+      messageArray.push({ message: newMessage, user: "me" });
+      setNewMessage("");
+    }
+    addMessageBot();
+  }
+
+  function addMessageBot() {
+    messageArray.push({ message: "Victor si es un mamaguevo", user: "bot" });
+    setNewMessage("");
+  }
 
   return (
     <>
       <div className="chat-frame">
-        <Chat user="me" message="Hello, how are you?" />
-        <Chat user="bot" message="Hi, I'm fine, thanks!" />
+        {messageArray.map((message, index) => (
+          <Chat key={index} user={message.user} message={message.message} />
+        ))}
       </div>
       <div className="write-bar">
-        <input placeholder="Write a message..." className="write-bar-input" />
-        <button className="write-bar-button"> {">"} </button>
+        <div className="input-container">
+          <input
+            placeholder="Write a message..."
+            className="write-bar-input"
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+          />
+          <button className="write-bar-button" onClick={addMessageUser}>
+            {">"}
+          </button>
+        </div>
       </div>
     </>
   );
